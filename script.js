802a0408caf0af6b8a912c99f8120f688527f2e7
bigosofts <abdullah.limonbau@gmail.com>
2024-11-16 23:07:20 +0600

    Javascript Basics class1

---
+++ script.js
@@ -0,0 +1,11 @@
+// function bulbon() {
+//   document.getElementById("bulb").src = "pic_bulbon.gif";
+// }
+
+// function bulboff() {
+//   document.getElementById("bulb").src = "pic_bulboff.gif";
+// }
+
+console.log(10 + 10);
+alert("hello");
+
