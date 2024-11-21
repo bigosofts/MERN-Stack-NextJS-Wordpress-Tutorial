3e93934ca6c987f0e570d2d8f2f62e72fe3fb705
bigosofts <abdullah.limonbau@gmail.com>
2024-11-21 20:34:33 +0600

    Advanced data type

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 function removeClass() {
-  document.getElementsByClassName("rifat")[0].classList.remove("designed");
-}
+  const x = 10; //number
+  let y = "10"; //string
+  let z = true; //boolean
+  let a = NaN; //number hobe na
+  let b = undefined; // bostu nai
+  let c = null; // bostu ase kintu man nai
+  let d = Infinity;
 
-function addClass() {
-  document.getElementById("body");
+  let limon = {
+    name: "limon",
+    age: 29,
+    service: "lecturer",
+  };
+
+  let classNine = [
+    {
+      name: "limon",
+      age: 29,
+      service: "lecturer",
+    },
+    {
+      name: "rifat",
+      age: 18,
+      service: "lecturer",
+    },
+    {
+      name: "yahia",
+      age: 24,
+      service: "lecturer",
+    },
+  ]; //array
+
+  // alert(numberofStudents[4]);
+
+  let rifat = {
+    name: "limon",
+    age: 29,
+    service: "lecturer",
+    marks: [2, 5, 8, 10, 14],
+  };
+
+  alert(rifat.marks[4]);
 }
+// function addClass() {
+//   document.getElementById("body");
+// }
 
 // console.log(10 + 10);
 // alert("hello");
