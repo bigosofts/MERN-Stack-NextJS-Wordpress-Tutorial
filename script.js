f5e5c9a9b7e74e8ad487a7a5f7acb61a1da741a6
bigosofts <abdullah.limonbau@gmail.com>
2024-12-02 23:17:06 +0600

    Array Iteration

--- script.js
+++ script.js
@@ -70,23 +70,79 @@ function yahia() {
 
 // yahia();
 
-let students = [
-  {
-    name: "limon",
-    roll: 1,
-    sibling: ["a", "b"],
-    getData: function () {
-      console.log(this.name + " " + this.roll);
-    },
-  },
-  {
-    name: "rifat",
-    roll: 2,
-    sibling: ["sumaiya", "d"],
-  },
-];
+// let students = [
+//   {
+//     name: "limon",
+//     roll: 1,
+//     sibling: ["a", "b"],
+//     getData: function () {
+//       console.log(this.name + " " + this.roll);
+//     },
+//   },
+//   {
+//     name: "rifat",
+//     roll: 2,
+//     sibling: ["sumaiya", "d"],
+//   },
+// ];
 
 //loop
-for (let i = 1; i <= 30; i++) {
-  document.write("<br> "+i);
-}
+// for (let i = 1; i <= 30; i++) {
+//   document.write("<br> " + i);
+// }
+
+let i = 11;
+
+// while (i < 10) {
+//   document.write("<br> " + i);
+//   i++;
+// }
+
+// do {
+//   document.write("<br> " + i);
+//   i++;
+// } while (i < 10);
+// {
+//   document.write("<br> " + i);
+// }
+let rifat = {
+  name: "rifat",
+  age: 18,
+  forEach: function () {
+    console.log(this.name);
+  },
+};
+
+let students = [1, 2, 10, 20];
+
+//array loop - iteration
+// students.forEach(function (item) {
+//   document.write(item + "<br>");
+// });
+
+// students.find(function (item) {
+//   document.write(item);
+// })
+
+// students.map(function (item) {
+//   document.write(item * 2);
+// });
+
+// students.filter(function (item) {
+//   item == "yahia" ? document.write(item) : "";
+// });
+
+// students.some(function (item) {
+//   item == "yahia" ? true : false;
+// });
+
+// students.every(function (item) {
+//   item == "yahia" ? true : false;
+// });
+
+const numbers = [45, 4, 9, 16, 25];
+let sum = numbers.reduce(function myFunction(total, item) {
+  return total + item;
+});
+
+document.getElementById("catch2").innerHTML = "The sum is " + sum;
