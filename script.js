767fc543e15f9d5f3e46ff986f7059f2f3905689
bigosofts <abdullah.limonbau@gmail.com>
2024-12-19 22:15:40 +0600

    Search in js

--- script.js
+++ script.js
@@ -1,215 +1,32 @@
-// function removeClass() {
-//   const x = 10; //number
-//   let y = "10"; //string
-//   let z = true; //boolean
-//   let a = NaN; //number hobe na
-//   let b = undefined; // bostu nai
-//   let c = null; // bostu ase kintu man nai
-//   let d = Infinity;
-//   let rifat = { name: "rifat", age: 20, eyeColor: "#000", baby: [5, 6, 7, 8] };
-
-//   rifat.baby[3]; //8
-//   // let limon = {
-//   //   name: "limon",
-//   //   age: 29,
-//   //   service: "lecturer",
-//   // };
-
-//   let classNine = [
-//     {
-//       name: "limon",
-//       age: 29,
-//       service: "lecturer",
-//     },
-//     {
-//       name: "rifat",
-//       age: 18,
-//       service: "lecturer",
-//     },
-//     {
-//       name: "yahia",
-//       age: 24,
-//       service: "lecturer",
-//     },
-//   ]; //array
-
-//   // alert(numberofStudents[4]);
-
-//   let car = {
-//     name: "limon", //property
-//     model: 29,
-//     weight: "lecturer",
-//     color: [2, 5, 8, 10, 14],
-//     getColor: function () {
-//       alert(this.name);
-//     }, //method
-//   };
-//   car.getColor();
-
-//   let set = "name";
-
-//   // alert(car["name"])
-
-//   car.getColor();
-// }
-// function addClass() {
-//   document.getElementById("body");
-// }
-
-// console.log(10 + 10);
-// alert("hello");
-
-// let a = 10; //assignment
-// let b = 20 % 8; //modulas
-// let c = 10 < 20; // comparision
-// let d = d--; //increment
-// let e = 5 ** 2;
-
-// function yahia() {
-//   let x = 10;
-//   let z = x * 100;
-//   alert(z);
-// }
-
-// yahia();
-
-// let students = [
-//   {
-//     name: "limon",
-//     roll: 1,
-//     sibling: ["a", "b"],
-//     getData: function () {
-//       console.log(this.name + " " + this.roll);
-//     },
-//   },
-//   {
-//     name: "rifat",
-//     roll: 2,
-//     sibling: ["sumaiya", "d"],
-//   },
-// ];
-
-//loop
-// for (let i = 1; i <= 30; i++) {
-//   document.write("<br> " + i);
-// }
-
-// let i = 11;
-
-// while (i < 10) {
-//   document.write("<br> " + i);
-//   i++;
-// }
-
-// do {
-//   document.write("<br> " + i);
-//   i++;
-// } while (i < 10);
-// {
-//   document.write("<br> " + i);
-// }
+//constructor function
 // let rifat = {
 //   name: "rifat",
 //   age: 18,
-//   forEach: function () {
-//     console.log(this.name);
-//   },
 // };
 
-// function addition(rifatAge, yahiaAge) {
-//   alert(rifatAge + yahiaAge); //parameter
-// }
+// let yahia = {
+//   name: "yahia",
+//   age: 26,
+// };
 
-// addition(18, 25); //argument
-
-// let students = [1, 2, 10, 20];
-
-//array loop - iteration
-
-// students.forEach(function (item) {
-//   document.write(item + "<br>");
-// });
-
-// students.find(function (item) {
-//   document.write(item);
-// })
-
-// students.map(function (item) {
-//   document.write(item * 2);
-// });
-
-// students.filter(function (item) {
-//   item == "yahia" ? document.write(item) : "";
-// });
-
-// students.some(function (item) {
-//   item == "yahia" ? true : false;
-// });
-
-// students.every(function (item) {
-//   item == "yahia" ? true : false;
-// });
-
-// const numbers = [45, 4, 9, 16, 25];
-
-// numbers.forEach();
-// let sum = numbers.reduce(function myFunction(total, item) {
-//   return total + item;
-// });
-// let classe = 10;
-
-// if (classe == 12) {
-//   alert("রিফাত দ্বাদশ শ্রেনীতে পড়ে  ");
-// } else if (classe == 11) {
-//   alert("রিফাত একাদশ  শ্রেনীতে পড়ে  ");
-// }else if (classe == 11) {
-//   alert("রিফাত একাদশ  শ্রেনীতে পড়ে  ");
-// }else if (classe == 11) {
-//   alert("রিফাত একাদশ  শ্রেনীতে পড়ে  ");
-// }else if (classe == 11) {
-//   alert("রিফাত একাদশ  শ্রেনীতে পড়ে  ");
-// }else if (classe == 11) {
-//   alert("রিফাত একাদশ  শ্রেনীতে পড়ে  ");
-// } else {
-//   alert("রিফাত পড়াশোনাই করে না  ");
-// }
-
-// 20 == 30 ? alert("কেমন আছেন? ") : alert("ভালো নাই"); //ternary operator
-
-// let database = {
-//   name: "Limon",
+// let limon = {
+//   name: "limon",
 //   age: 30,
-//   education: "Lecturer",
 // };
 
-// function fetchData(cv){
-//   let name = database.name;
-//   let age = database.age;
-//   let education = database.education;
-//   cv(name, age, education);
+// function Person(age, name) {
+//   this.name = name;
+//   this.age = age;
 // }
 
-// function CVMaker(name, age, education) {
-//   document.getElementById(
-//     "catch2"
-//   ).innerHTML = `Name: ${name}<br/> Age: ${age}<br/> Education: ${education}`;
-// }
+// let limon = new Person(30, "limon");
+// let yahia = new Person(26, "yahia");
+// let rifat = new Person(18, "rifat");
 
-// fetchData(CVMaker);
+// alert(yahia.age);
 
-// let text = "Apple, Banana, Kiwi, Apple";
-// // let part = text.slice(7,13);
-// let text2 = text.replace("Apple", "Guava");
+let text = "Please locate where 'locate' occurs!";
 
-let text = "a,b,c,d,e,f";
-const myArray = text;
+let index = text.search("locate");
 
-function rifat(a, b) {
-  console.log(a);
-}
-
-let rifat = (a, b) => {
-  console.log(a);
-};
-
-console.log(myArray);
+document.write(index);
