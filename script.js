b67ac4ed55922e689494eaa4c7ef014a87e506a6
bigosofts <abdullah.limonbau@gmail.com>
2024-12-25 23:11:18 +0600

    Javascript scope

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-//Javascript Closure
-
-// function hello() {
-//   var a = 10;
-//   var b = 20;
-//   function rifat() {
-//     console.log("Rifat Age " + a + b);
-//   }
-//   return rifat;
-// }
-
-// var rifat = hello();
-
-// console.dir(rifat);
-
-//javascript prototype
-
 //function Constructor
 
-function Rifat(name, age) {
-  this.name = name;
-  this.age = age;
+// function Rifat(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// var rifatObject = new Rifat("rifat", 18);
+// var yahiaObject = new Rifat("yahia", 25);
+
+// Rifat.prototype.fullName = function () {
+//   console.log(this.name + " " + this.age);
+// };
+
+// rifatObject.fullName();
+// yahiaObject.fullName();
+
+//Javascript Scope
+
+var a = 10;
+
+function rifat() {
+  let a  = 30;
+  function yahia() {
+    console.log("Rifat age is: " + a);
+  }
+  yahia();
 }
 
-var rifatObject = new Rifat("rifat", 18);
-var yahiaObject = new Rifat("yahia", 25);
+rifat();
 
-console.log(yahiaObject);
+// বাইরে যদি ভ্যারিএবল লিখি তাইলে গ্লোবাল স্কোপ 
 
-var a = new Object(); //{}
+// ফাংশনের ভিতর যদি ডিক্লেয়ার না করে আসাইন করে দাও দাও সেটা সেটা হবে গ্লোবাল স্কোপ
 
-var b = new Array(); //[]
+// ফাংশনের ভিতরে ডিক্লেয়ার প্লাস আসাইন করা হলে সেটা হবে লোকাল স্কোপ।
 
-var c = new String();
+// কোন ফাংশনের পেটের ভিতর আরো ফাংশন থাকলে তারাও সেই ভ্যারিএবল স্কোপ পাবে এটাকে বলে লেক্সিকাল স্কোপ।
 
-var d = new Boolean();
+
+// var ফাংশন স্কোপ মেইন্টেইন করে , তবে let and const ব্লক স্কোপ মেইন্টেইন করে । 
+
+// let কে রিএসাইন করা যায়, তবে const  কে রিএসাইন করা যায় না । 
+
