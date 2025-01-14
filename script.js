da91093273a490ca92ccbc4fc3081d73498e5097
bigosofts <abdullah.limonbau@gmail.com>
2025-01-14 21:31:03 +0600

    ReactJS

--- script.js
+++ script.js
@@ -1,60 +1,43 @@
-//javascript inheritence
-
-//Easy inheritence
-
-//Moderate inheritence
-
-// Intermediate inheritance
-
-// function Human(eye, leg) {
-//   this.eye = eye;
-//   this.leg = leg;
+// var number = 0;
+// var number2 = 0;
+// function increment() {
+//   number++;
+//   var element = document.querySelector("#counter");
+//   element.innerHTML = number;
 // }
 
-// function Rifat(age, name, eye, leg) {
-//   Human.call(this, eye, leg);
-//   this.age = age;
-//   this.name = name;
+// function increment2() {
+//   number2++;
+//   var element = document.querySelector("#counter2");
+//   element.innerHTML = number2;
 // }
-// Rifat.prototype.getAge = function () {
-//   console.log("Age is " + this.age);
-// };
 
-// let rifat = new Rifat(18, "Rifat", "red", "Long");
-// let yahia = new Rifat(28, "Yahia", "black", "short");
+var rootElement = document.querySelector("#root");
 
-// console.log(yahia);
+function Button() {
+  const [counter, setCounter] = React.useState(0);
 
-// let rifat = {
-//   name: "Rifat",
-//   age: 18,
-//   getAge: function () {
-//     console.log(this.age);
-//   },
-// };
-
-// let yahia = Object.create(rifat);
-
-// console.log(yahia);
-
-class Human {
-  constructor(leg, eye) {
-    this.eye = eye; // Assign the name
-    this.leg = leg; // Assign the age
-  }
-  getAge() {
-    console.log("Age is " + this.age);
-  }
+  return (
+    <div>
+      <h1>
+        <span>{counter}</span> {/* Display the counter value */}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>Add+</button>{" "}
+      {/* Update counter on click */}
+    </div>
+  );
 }
 
-class Rifat extends Human {
-  constructor(age, name, leg, eye) {
-    super(leg, eye);
-    this.name = name;
-    this.age = age;
-  }
+function Main() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
 }
 
-let rifat = new Rifat(25, "Rifat", 2, "brown");
-
-
+ReactDOM.render(<Main />, rootElement);
