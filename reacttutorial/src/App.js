b7fe611804ea1778892a50c954b7d1a067dd411c
bigosofts <abdullah.limonbau@gmail.com>
2025-01-27 23:51:51 +0600

    Props in react

--- reacttutorial/src/App.js
+++ reacttutorial/src/App.js
@@ -1,47 +1,97 @@
-import ButtonComponent from "./components/Button";
-
 //functional COmponent
 
-function App() {
-  return (
-    <>
-      <ButtonComponent text="Click Me" color="Red" />
-      <ButtonComponent text="Read More" color="green" />
-      <ButtonComponent text="Go Back" color="purple" />
-    </>
-  );
-}
+import ButtonComponent from "./components/Button";
+import React, { useEffect, useState } from "react";
+// import { useState } from "react";
 
-//class component
+// export default function App() {
+//   let [text, setText] = useState("This is initial Rifat Text"); // ["This is initial Text", ()=> {}]
 
-// class App extends React.Component {
+//   return (
+//     <>
+//       <h1> {text} </h1>
+//       <button onClick={() => setText("This is limon")}>Click me</button>
+//     </>
+//   );
+// }
+
+// export default class App extends React.Component {
+//   state = {
+//     text: "This is Rifat Text 1",
+//   };
+
+//   onHandle() {
+//     this.setState({
+//       text: "This is Yahia Text",
+//     });
+//   }
+
 //   render() {
 //     return (
-//       <div>
-//         <div>Yahia</div>
-//       </div>
+//       <>
+//         <h1> {this.state.text}</h1>
+//         <button onClick={()=> this.onHandle()}>Click Me</button>
+//       </>
 //     );
 //   }
 // }
 
-export default App;
+// export default function App() {
+//   const [text, setText] = useState("");
+//   const [text2, setText2] = useState("This is text 2");
 
-// let a = {
-//   type: "div",
-//   props: {
-//     children: {
-//       type: "div",
-//       props: { children: "Rifat" },
-//     },
-//   },
-// };
+//   return (
+//     <>
+//       <h1>{text}</h1>
+//       <h1>{text2}</h1>
+//       <button
+//         onClick={() => {
+//           setText2("This is Text 3");
+//           setText("");
+//         }}
+//       >
+//         Press Me
+//       </button>
+//       <ButtonComponent text="click me" color="red" />
+//     </>
+//   );
+// }
 
-// let a = {
-//   type: "div",
-//   props: {
-//     children: {
-//       type: "div",
-//       props: { children: "Yahia" },
-//     },
-//   },
-// };
+export default class App extends React.Component {
+  state = {
+    text: "",
+    text2: "This is text 2",
+  };
+
+  componentDidMount() {
+    let delay = () => {
+      setTimeout(() => {
+        this.setState({
+          text: "Data Found",
+        });
+      }, 5000);
+    };
+
+    delay();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.text}</h1>
+        <h1>{this.state.text2}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              text2: "This is Text 3",
+              text: "",
+            });
+          }}
+        >
+          Press Me
+        </button>
+        <ButtonComponent text="click me" color="red" />
+      </>
+    );
+  }
+}
--- reacttutorial/src/components/Button.js
+++ reacttutorial/src/components/Button.js
@@ -1,10 +1,21 @@
 import "./Button.css";
-function ButtonComponent({ text, color }) {
-  return (
-    <button style={{ backgroundColor: color }} className="btn">
-      {text}
-    </button>
-  );
+import React from "react";
+// function ButtonComponent({ text, color }) {
+//   return (
+//     <button style={{ backgroundColor: color }} className="btn">
+//       {text}
+//     </button>
+//   );
+// }
+
+class ButtonComponent extends React.Component {
+  render() {
+    return (
+      <button style={{ backgroundColor: this.props.color }} className="btn">
+        {this.props.text}
+      </button>
+    );
+  }
 }
 
 export default ButtonComponent;
